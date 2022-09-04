4cf0b9cf0bf5a0ef1bb75eb3c4cc16650fe3fbac
bunic123 <bunic.fon@gmail.com>
2022-09-05 01:44:04 +0200

    Login.js - strana za login, niz users

---
+++ src/strane/Login.js
@@ -0,0 +1,16 @@
+function Login() {
+
+    const users = [
+        { username: 'goran', password: 'goran', email: 'goran@gmail.com' },
+        { username: 'marko', password: 'marko', email: 'marko@gmail.com' },
+        { username: 'nikola', password: 'nikola', email: 'nikola@gmail.com' }
+    ];
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Login;
