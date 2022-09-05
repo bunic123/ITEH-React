ff41e0b6b84c6c77e9179906727f1e0acd85c01a
bunic123 <bunic.fon@gmail.com>
2022-09-05 02:20:26 +0200

    Pretraziknjige funkcija koja vraća sve knjige koje sadrže unetu vrednost u nazivu

--- src/strane/Knjige.js
+++ src/strane/Knjige.js
@@ -3,18 +3,38 @@ import { useState } from 'react'
 function Knjige() {
 
     const [pretraga, setPretraga] = useState('');
+    const [knjige, setKnjige] = useState([]);
+
 
     function handlePretraga(e) {
         setPretraga(e.target.value)
     }
 
 
+    function pretraziKnjige() {
+
+        const options = {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': '4916172c11msh0beee19c8846ca4p127267jsn0bc7bcd7fc43',
+                'X-RapidAPI-Host': 'hapi-books.p.rapidapi.com'
+            }
+        };
+
+        fetch(`https://hapi-books.p.rapidapi.com/search/${pretraga}`, options)
+            .then(response => response.json())
+            .then(response => setKnjige(response))
+            .catch(err => console.error(err));
+    }
+
+    console.log(knjige)
+
     return (
         <div>
             <h1 className='mt-4'>Pretrazi knjige</h1>
             <div className="pretrazi">
                 <input type="text" className="form-control" onChange={handlePretraga} value={pretraga} />
-                <button className="btn btn-primary mt-2">Pretraga</button>
+                <button onClick={pretraziKnjige} className="btn btn-primary mt-2">Pretraga</button>
             </div>
         </div>
     )
